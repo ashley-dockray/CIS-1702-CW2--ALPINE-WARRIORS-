54d598489e98575cb6603319e8f5ff7c7e54d065
Frederick Bullen <freddybullen@icloud.com>
2025-12-28 13:52:41 +0000

    Add search_item function which searches the inventorry data for an item of a specified name

--- search.py
+++ search.py
@@ -1,1 +1,17 @@
+inventory_json= [{"id": 101, "name": "Wireless Mouse", "price": 25.50, "stock": 15},
+{"id": 102, "name": "Mechanical Keyboard", "price": 120.00, "stock":
+8},
+{"id": 103, "name": "USB-C Hub", "price": 45.99, "stock": 0},
+{"id": 104, "name": "Monitor Stand", "price": 29.99, "stock": 12}]
 
+
+def search_item(Name, inventory_data):
+    for items in inventory_data:
+        if items['name'] == Name:
+            print (items)
+        else:
+            None
+
+
+if __name__=="__main__":
+    search_item('USB-C Hub', inventory_json)
