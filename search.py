6b1b45b562d97d9c9bbf8526414008292043e07b
Frederick Bullen <freddybullen@icloud.com>
2026-01-06 17:00:55 +0000

    removed the while loop and added .lower() and .strip() functions instead
    the while loop had potential to put the user into an infinite loop, when putting the code together this was the method that appeared most compatible.

--- search.py
+++ search.py
@@ -1,7 +1,6 @@
-def search_item( inventory_data): # imports the inventory data
-    while True: 
-        name = input('enter product name') # allows user to enter the name of the product
-        for item in inventory_data: # itterates through the data in search for the name
-            if item['name'] == name:
-                return item  # retruns the item of matching name 
-        print('sorry item not found, Please try again')
+def search_item(inventory_data):
+    name = input('Enter product name: ').strip()
+    for item in inventory_data:
+        if item['name'].lower() == name.lower():
+            return item
+    return None
