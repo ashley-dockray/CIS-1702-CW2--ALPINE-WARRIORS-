68f232d4cb0ad40fe1c2525b5bdb9ec6706a17ae
Frederick Bullen <freddybullen@icloud.com>
2026-01-06 18:26:34 +0000

    syntax error

--- search.py
+++ search.py
@@ -2,5 +2,5 @@ def search_item(inventory_data):
     name = input('Enter product name: ').strip() # saves the product name as an input, removing unnecisary spacings
     for item in inventory_data: #iterates through the data in search for item with that name 
         if item['name'].lower() == name.lower(): #compares each item with the name 
-            return item returnes the correct item
+            return item #returnes the correct item
     return None
