09ab2b7e5ab916b1be478aba0536baae1c111745
Frederick Bullen <freddybullen@icloud.com>
2026-01-06 17:10:06 +0000

    added comments to describe the code
    Refactor search_item function to improve readability.

--- search.py
+++ search.py
@@ -1,6 +1,6 @@
 def search_item(inventory_data):
-    name = input('Enter product name: ').strip()
-    for item in inventory_data:
-        if item['name'].lower() == name.lower():
-            return item
+    name = input('Enter product name: ').strip() # saves the product name as an input, removing unnecisary spacings
+    for item in inventory_data: #iterates through the data in search for item with that name 
+        if item['name'].lower() == name.lower(): #compares each item with the name 
+            return item returnes the correct item
     return None
