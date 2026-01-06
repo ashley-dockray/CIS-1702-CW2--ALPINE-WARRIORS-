587071d4f8601ee7ea62595443b620a434a7298c
Frederick Bullen <freddybullen@icloud.com>
2026-01-06 15:22:54 +0000

    Error Handeling
    I have added a while loop which prints a message if the name is not found and allows the user to re-enter the name.

--- search.py
+++ search.py
@@ -1,6 +1,7 @@
 def search_item( inventory_data): # imports the inventory data
-    name = input('enter product name') # allows user to enter the name of the product
-    for item in inventory_data: # itterates through the data in search for the name
-        if item['name'] == name:
-            return item  # retruns the item of matching name 
-    return None
+    while True: 
+        name = input('enter product name') # allows user to enter the name of the product
+        for item in inventory_data: # itterates through the data in search for the name
+            if item['name'] == name:
+                return item  # retruns the item of matching name 
+        print('sorry item not found, Please try again')
